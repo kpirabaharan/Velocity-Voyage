698c563e3e915260940ede205a9dfa03fddf4f12
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-04-23 21:02:36 -0400

    Added Score Text and Stopped Increment on Crash
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- Assets/Scripts/LogScript.cs
+++ Assets/Scripts/LogScript.cs
@@ -3,15 +3,18 @@ using UnityEngine;
 public class LogScript : MonoBehaviour
 {
     PlayerMovement playerMovement;
+    LogicScript logicScript;
 
     // Start is called before the first frame update
     void Start()
     {
         playerMovement = GameObject.FindObjectOfType<PlayerMovement>();
+        logicScript = GameObject.FindObjectOfType<LogicScript>();
     }
 
     private void OnTriggerEnter(Collider other)
     {
         playerMovement.runSpeed = 0;
+        logicScript.SetIsGameOver();
     }
 }
---
+++ Assets/Scripts/LogicScript.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+
+public class LogicScript : MonoBehaviour
+{
+    private float score;
+    public bool isGameOver = false;
+    public TextMeshProUGUI scoreText;
+    public GameObject gameOverScreen;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        scoreText.text = "Score: " + score.ToString("F0");
+    }
+
+    private void FixedUpdate()
+    {
+        if(!isGameOver)
+            IncrementScore();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        scoreText.text = "Score: " + score.ToString("F0");
+        if (isGameOver)
+            OnGameOver();
+    }
+
+    private void IncrementScore()
+    {
+        score += Time.deltaTime * 10;
+    }
+
+    public void SetIsGameOver()
+    {
+        isGameOver = true;
+    }
+
+    private void OnGameOver()
+    {
+        gameOverScreen.SetActive(true);
+    }
+}
